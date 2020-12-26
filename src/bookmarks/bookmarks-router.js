457fade635298b33861a2b3d581f85d44003a06a
Ryan Chase <ryan@chasery.com>
2020-12-26 12:05:35 -0800

    Add bookmark post endpoint with validation

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { v4: uuid } = require("uuid");
+const { isWebUri } = require("valid-url");
 const logger = require("../logger");
 const bookmarks = require("../store");
 
@@ -13,7 +14,45 @@ bookmarksRouter
     res.json(bookmarks);
   })
   .post(bodyParser, (req, res) => {
-    // Handle request to create a new bookmark
+    // Handle request to create a new bookmark (Title, URL, Rating, Description)
+    // Req input is Title, URL, and Rating
+    for (const field of ["title", "url", "rating"]) {
+      if (!req.body[field]) {
+        logger.error(`${field} is required`);
+        return res.status(400).send(`'${field}' is required`);
+      }
+    }
+
+    const { title, url, rating, description } = req.body;
+
+    // Url validation
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url '${url}' supplied`);
+      return res.status(400).send(`'url' must be a valid URL`);
+    }
+
+    // Rating validaiton
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+      logger.error(`Invalid rating '${rating}' supplied`);
+      return res.status(400).send(`'rating' must be a number between 0 and 5`);
+    }
+
+    const bookmark = {
+      id: uuid(),
+      title,
+      url,
+      rating,
+      description,
+    };
+
+    bookmarks.push(bookmark);
+
+    logger.info(`Bookmark with id ${bookmark.id} created`);
+
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+      .json(bookmark);
   });
 
 bookmarksRouter
